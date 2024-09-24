71bfc8e0b0a2a395151fb0b8a6cec7ebf211181b
lascion <amo.lascion@nal.network>
2024-09-24 17:49:14 +0800

    add RegNFT

---
+++ token/ERC721/RegNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract RegNFT is ERC721, ERC721Burnable, Ownable {
+    constructor(string memory name, string memory symbol, address initialOwner)
+        ERC721(name, symbol)
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
